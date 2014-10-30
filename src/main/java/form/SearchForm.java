71fecf2ca7df726e950195c252f1d7f96e4c616a
dpochtarev <ts4dmitry@yandex.ru>
2014-10-30 21:02:07 +0300

    Сделал поиск

---
+++ src/main/java/form/SearchForm.java
@@ -0,0 +1,11 @@
+package form;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dmitry
+ * Date: 30.10.14
+ * Time: 12:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchForm {
+}
